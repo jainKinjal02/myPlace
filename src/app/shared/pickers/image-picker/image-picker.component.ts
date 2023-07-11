7e41f48e8b4bac63e93143430826a08d6efbeb6d
Kinjal Jain <jainkinjal021196@gmail.com>
2023-07-11 17:10:31 +0530

    Added Camera device feature

---
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType } from '@capacitor/camera';
+
+@Component({
+  selector: 'app-image-picker',
+  templateUrl: './image-picker.component.html',
+  styleUrls: ['./image-picker.component.scss'],
+})
+export class ImagePickerComponent  implements OnInit {
+  selectedImage: string;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  onPickImage(){
+
+  }
+
+}
