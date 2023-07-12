9545f8cbee92b0bfca2eb66f35e475dbc248b13d
Kinjal Jain <jainkinjal021196@gmail.com>
2023-07-12 13:58:20 +0530

    Image upload added

--- src/app/shared/pickers/image-picker/image-picker.component.ts
+++ src/app/shared/pickers/image-picker/image-picker.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit ,EventEmitter, Output} from '@angular/core';
+import { Component, OnInit ,EventEmitter, Output, ViewChild, ElementRef, Input} from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Capacitor } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-image-picker',
@@ -8,10 +9,12 @@ import { Capacitor } from '@capacitor/core';
   styleUrls: ['./image-picker.component.scss'],
 })
 export class ImagePickerComponent  implements OnInit {
-  @Output() imagePick = new EventEmitter<string>();
+  @ViewChild('filePicker') filePickerRef: ElementRef<HTMLInputElement>;
+  @Output() imagePick = new EventEmitter<string | File>();
+  @Input() showPreview = false;
   selectedImage: string;
-
-  constructor() { }
+  usePicker = false;
+  constructor(private platform : Platform) { }
 
 
 checkPlatformForWeb(){
@@ -21,7 +24,12 @@ checkPlatformForWeb(){
 
 }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if((this.platform.is('mobile') && !this.platform.is('hybrid'))
+      || this.platform.is('desktop')){
+      this.usePicker = true;
+    }
+  }
 
   async onPickImage(){
     if(!this.checkPlatformForWeb()){
@@ -30,21 +38,39 @@ checkPlatformForWeb(){
         quality: 90, // there are many options ,check official site
         allowEditing: true,
 				source: CameraSource.Prompt,
-        resultType: CameraResultType.DataUrl,
+        resultType: CameraResultType.Base64,
         correctOrientation: true,
         width: 600
       }).then(image => {
-        this.selectedImage=image.dataUrl;
-        this.imagePick.emit(image.dataUrl);
+        this.selectedImage=image.base64String;
+        this.imagePick.emit(image.base64String);
       }).catch(error=>{
         console.log(error);
         return false;
       });
     }else{
-      console.log('Camera is not available for web');
+      this.filePickerRef.nativeElement.click();
+      return;
+      //console.log('Camera is not available for web');
     }
 
 
   }
+
+  onFileChosen(event: Event){
+    const pickedFile = (event.target as HTMLInputElement).files[0];
+    if(!pickedFile){
+      return;
+    }
+    const fr = new FileReader();
+    fr.onload = () =>{
+      const dataUrl = fr.result.toString();
+      this.selectedImage = dataUrl;
+      this.imagePick.emit(pickedFile);
+    };
+    fr.readAsDataURL(pickedFile);
+  }
+
+
 
 }
